6baea4e5749d289c6343e35473ea05fa6fbab5e1
Nikola Draskovic <draskovic26z@gmail.com>
2024-10-30 20:24:05 +0100

    creates a lib crate

---
+++ lib/src/req/create_transfers.rs
@@ -0,0 +1,37 @@
+use std::ptr::null_mut;
+
+use tigerbeetle_unofficial_sys::{tb_packet_t, tb_transfer_t, TB_OPERATION, TB_PACKET_STATUS};
+
+use crate::{
+    consts::MAX_TRANSFERS_PER_MSG, err::TbPacketErr, resp::create_transfers::CreateTransfersResp,
+    Client,
+};
+
+impl Client {
+    /// Caveats:
+    /// - those of [`Self::request`] apply
+    /// - `accounts.len()` must not exceed [`MAX_TRANSFERS_PER_MSG`]
+    pub async fn create_transfers(
+        &self,
+        transfers: &[tb_transfer_t],
+    ) -> Result<CreateTransfersResp, TbPacketErr> {
+        assert!(transfers.len() <= MAX_TRANSFERS_PER_MSG);
+        let packet = tb_packet_t {
+            operation: TB_OPERATION::TB_OPERATION_CREATE_TRANSFERS as u8,
+            status: TB_PACKET_STATUS::TB_PACKET_OK as u8,
+            data_size: core::mem::size_of_val(transfers) as u32,
+            // cast-safety: request should not modify data but generated bindings take *mut
+            data: transfers.as_ptr().cast_mut().cast(),
+            // set by [`Req::poll()`]
+            user_data: null_mut(),
+            // dont-cares?
+            next: null_mut(),
+            batch_next: null_mut(),
+            batch_tail: null_mut(),
+            batch_size: 0,
+            batch_allowed: 0,
+            reserved: [0u8; 7],
+        };
+        self.request(packet).await.map(CreateTransfersResp)
+    }
+}
---
+++ lib/src/resp/lookup_accounts.rs
@@ -0,0 +1,23 @@
+use tigerbeetle_unofficial_sys::tb_account_t;
+
+use super::RespBuf;
+
+#[repr(transparent)]
+#[derive(Clone, Debug)]
+pub struct LookupAccountsResp(pub(crate) RespBuf);
+
+impl LookupAccountsResp {
+    #[inline]
+    pub const fn as_slice(&self) -> &[tb_account_t] {
+        let byte_slice = self.0.as_slice();
+        let len = byte_slice.len() / core::mem::size_of::<tb_account_t>();
+        unsafe { core::slice::from_raw_parts(byte_slice.as_ptr().cast(), len) }
+    }
+}
+
+impl AsRef<[tb_account_t]> for LookupAccountsResp {
+    #[inline]
+    fn as_ref(&self) -> &[tb_account_t] {
+        self.as_slice()
+    }
+}
